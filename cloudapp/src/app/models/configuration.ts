481f11776f91a4ead2814cf148f83a0eb9ab68b8
mgobat <45852490+mgobat@users.noreply.github.com>
2021-04-06 13:53:46 -0500

    Fix EAN13 barcode encoding support
    EAN must be set as EAN13.

--- cloudapp/src/app/models/configuration.ts
+++ cloudapp/src/app/models/configuration.ts
@@ -14,7 +14,7 @@ export class Templates {
   [key: string]: Template
 }
 
-export const barcodeFormats = ['CODE128' , 'CODE128A' , 'CODE128B' , 'CODE128C' , 'EAN' , 'UPC' , 'EAN8' , 'EAN5' , 'EAN2' , 'CODE39' , 'ITF14' , 'MSI' , 'MSI10' , 'MSI11' , 'MSI1010' , 'MSI1110' , 'pharmacode' , 'codabar'] as const;
+export const barcodeFormats = ['CODE128' , 'CODE128A' , 'CODE128B' , 'CODE128C' , 'EAN13' , 'UPC' , 'EAN8' , 'EAN5' , 'EAN2' , 'CODE39' , 'ITF14' , 'MSI' , 'MSI10' , 'MSI11' , 'MSI1010' , 'MSI1110' , 'pharmacode' , 'codabar'] as const;
 type BarcodeFormats = typeof barcodeFormats[number];
 
 export class Template {

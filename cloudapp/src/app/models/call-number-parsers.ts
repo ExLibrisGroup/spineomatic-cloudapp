f142bc9572188f6f02492d8a512549a9be6614d3
Josh Weisman <josh.m.weisman@gmail.com>
2021-08-10 17:04:22 +0300

    Add additional call number parser
    #32

--- cloudapp/src/app/models/call-number-parsers.ts
+++ cloudapp/src/app/models/call-number-parsers.ts
@@ -1,5 +1,5 @@
 export interface CallNumberParsers {
-  [key: string]: (val: string | Array<string>) => Array<string>
+  [key: string]: (val: string | Array<string>) => string | Array<string>
 }
 
 export const callNumberParsers: CallNumberParsers = {
@@ -7,4 +7,9 @@ export const callNumberParsers: CallNumberParsers = {
     if (Array.isArray(val)) val = val.join(' ');
     return val.split('/');
   },
+  'remove_shelving_info': val => {
+    if (Array.isArray(val)) val = val.join(' ');
+    const matches = val.match(/^[\w\s]*\/[\w\s]*\b/g);
+    return (matches && matches[0]) || val;
+  }
 }

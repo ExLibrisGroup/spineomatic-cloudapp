1eb99ba546f7676e78bcce2774afbae53235d24b
dongchch <chenchen.dong@exlibrisgroup.com>
2021-10-21 14:10:30 +0800

    Add a new parser into call-number-parsers
    The customer wants to print the part of call number before the first : character.
    So I add a new regex : (/^.*\/.*:/) to match the colon after '/'. After matching,remove the left 'colon'.

--- cloudapp/src/app/models/call-number-parsers.ts
+++ cloudapp/src/app/models/call-number-parsers.ts
@@ -11,5 +11,21 @@ export const callNumberParsers: CallNumberParsers = {
     if (Array.isArray(val)) val = val.join(' ');
     const matches = val.match(/[0-9A-Za-z\s\/\.\:\(\)\#\=\+\-]+/g);
     return ((matches && matches[0]) || val).split('/');
+  },
+  'remove_shelving_info_and_colon': val => {
+    if (Array.isArray(val)) val = val.join(' ');
+    const matches = val.match(/[0-9A-Za-z\s\/\.\:\(\)\#\=\+\-]+/g);
+    let strWithColon = (matches && matches[0]) || val;
+    if(strWithColon!= null && strWithColon.indexOf(":") != -1){
+      const matches_colon = strWithColon.match(/^.*\/.*:/);
+      let str = (matches_colon && matches_colon[0]) || strWithColon;
+      if(str != null && str.endsWith(":")){
+        str = str.substring(0,str.length-1);
+      }
+      return ((matches_colon && str) || strWithColon).split('/');
+    }else{
+      return ((matches && matches[0]) || val).split('/');
+    }
+    
   }
 }

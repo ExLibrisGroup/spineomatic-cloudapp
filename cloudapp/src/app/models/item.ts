ef7dadecbf1718778c37a754c27240dcf705d4e5
mgobat <45852490+mgobat@users.noreply.github.com>
2022-03-30 14:35:50 -0500

    Update eca-components, fix GitHub issue #63
    Update eca-components.
    Fix GitHub issue #63. Fix requires updating existing templates to use the correct field for item_data.description.

--- cloudapp/src/app/models/item.ts
+++ cloudapp/src/app/models/item.ts
@@ -29,7 +29,7 @@ export const LABEL_FIELDS = [
   'item_data.barcode',
   'item_data.call_no',
   'item_data.alt_call_no',
-  'item_data.issue_level_description',
+  'item_data.description',
   'item_data.edition',
   'item_data.imprint',
   'item_data.language',
